13e573345b230b8bf8e1884103b0461b7a1c6f0c
RH <rahim.heydarov@usi.ch>
2021-07-20 01:33:47 +0400

    ..

--- cpu_hog.py
+++ cpu_hog.py
@@ -15,7 +15,7 @@ def signal_handler(signal_, frame):
 
 
 def cpuhog(loop, sleep):
-    print("cpuhog", loop, sleep)
+    print("cpuhog", loop.value, sleep.value)
     while flag.value:
         looptimes = loop.value
         for i in range(looptimes):
